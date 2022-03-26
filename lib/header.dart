8e43f5bb24c2581025fac67326f398362f8e598c
Tal Spektor <talspektor@gmail.com>
2022-03-26 22:39:11 +0300

    Initial commit

---
+++ lib/header.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+class Header extends StatefulWidget {
+  const Header({Key? key}) : super(key: key);
+
+  @override
+  State<Header> createState() => _HeaderState();
+}
+
+class _HeaderState extends State<Header> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SliverList(
+      delegate: SliverChildListDelegate(
+        [
+          Stack(
+            children: [
+              Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    height: size.height / 5,
+                    decoration: const BoxDecoration(
+                        color: Colors.teal,
+                        borderRadius:
+                            BorderRadius.vertical(bottom: Radius.circular(45)),
+                        boxShadow: [
+                          BoxShadow(offset: Offset(0, 0), blurRadius: 2)
+                        ]),
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 20),
+                        Row(
+                          children: [
+                            const CircleAvatar(
+                              backgroundColor: Colors.white70,
+                              radius: 35,
+                              child: CircleAvatar(
+                                backgroundImage: AssetImage('images/dog.jpeg'),
+                                radius: 30,
+                              ),
+                            ),
+                            const SizedBox(width: 5),
+                            Column(
+                              children: [
+                                const Text(
+                                  'Wanted Jack',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18),
+                                ),
+                                Container(
+                                  padding: const EdgeInsets.all(4),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20),
+                                    color: Colors.black54,
+                                  ),
+                                  child: const Text(
+                                    'GOLD Member',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const Spacer(),
+                            const Text(
+                              '154 \$ CAN',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                ],
+              ),
+              Positioned(
+                bottom: 0,
+                child: Container(
+                  height: 50,
+                  width: size.width,
+                  child: Card(
+                    elevation: 3,
+                    margin: const EdgeInsets.symmetric(horizontal: 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        labelText: 'What does your belly want to eat?',
+                        suffixIcon: Icon(Icons.search),
+                        contentPadding: EdgeInsets.only(left: 20),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_hamburger/categories.dart';
+import 'package:flutter_app_hamburger/header.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(color: Colors.teal, centerTitle: true),
+        bottomAppBarColor: Colors.teal,
+        floatingActionButtonTheme:
+            const FloatingActionButtonThemeData(backgroundColor: Colors.teal),
+      ),
+      home: const Hamburger(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Hamburger extends StatefulWidget {
+  const Hamburger({Key? key}) : super(key: key);
+
+  @override
+  State<Hamburger> createState() => _HamburgerState();
+}
+
+class _HamburgerState extends State<Hamburger> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: const Text("Deliver Me"),
+            pinned: true,
+            leading: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.menu),
+            ),
+            actions: [
+              IconButton(
+                  onPressed: () {}, icon: const Icon(Icons.add_shopping_cart))
+            ],
+          ),
+          const Header(),
+          const Categories(),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                const Text(
+                  'Hamburger',
+                  style: TextStyle(fontSize: 300),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      extendBody: true,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.home),
+      ),
+      bottomNavigationBar: ClipRRect(
+        borderRadius: const BorderRadius.vertical(
+          top: Radius.circular(35),
+        ),
+        child: Container(
+          color: Colors.black38,
+          child: BottomAppBar(
+            shape: const CircularNotchedRectangle(),
+            child: Row(
+              children: [
+                const Spacer(),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.add_alert),
+                  color: Colors.white,
+                ),
+                const Spacer(),
+                const Spacer(),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.turned_in),
+                  color: Colors.white,
+                ),
+                const Spacer(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
